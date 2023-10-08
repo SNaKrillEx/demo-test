1d271950e6dd1b0e6f077c61f55e6145e5620e92
wzf <2388046176@qq.com>
2023-10-08 23:41:31 +0800

    Initial commit

---
+++ jstest/test01.js
@@ -0,0 +1,16 @@
+let a = "a"
+let b = "b"
+let c = "c"
+switch (c){
+    case "a":
+        console.info("1")
+        break
+    case "b":
+        console.info("2")
+        break
+    case "c":
+        console.info("3")
+        break
+    default:
+        console.info("4")
+}
